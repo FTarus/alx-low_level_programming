015ffe5862c639ba1d1d3da3686cb13d438fd326
Your Name <you@example.com>
2022-09-10 14:09:05 -0700

    alphABET
    mandatory
    
    git commit -malphABET
    
    mandatory
    
    git commit -malphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,15 +12,15 @@ int main(void)
 {
 char ch;
 
-for (ch = 'a' ; ch <= 'z' ; ch++)
-    {
-      putchar(ch);
-    }
-for (ch = 'A' ; ch <= 'Z' ; ch++)
-    {
-      putchar(ch);
-    }
-  putchar('\n');
+ for (ch = 'a' ; ch <= 'z' ; ch++)
+   {
+     putchar(ch);
+   }
+ for (ch = 'A' ; ch <= 'Z' ; ch++)
+   {
+     putchar(ch);
+   }
+ putchar('\n');
 
-return (0);
+ return (0);
 }
