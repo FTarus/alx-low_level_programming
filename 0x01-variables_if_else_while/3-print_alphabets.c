c0513c48805b7192906677c513acb59e22d4dbae
Your Name <you@example.com>
2022-09-12 15:03:39 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,16 +11,18 @@
 int main(void)
 {
 char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
 
- for (ch = 'a' ; ch <= 'z' ; ch++)
-   {
-     putchar(ch);
-   }
- for (ch = 'A' ; ch <= 'Z' ; ch++)
-   {
-     putchar(ch);
-   }
- putchar('\n');
+{
+putchar(ch);
+}
 
- return (0);
+for (ch = 'A' ; ch <= 'Z' ; ch++)
+
+{
+putchar(ch);
+}
+
+putchar('\n');
+return (0);
 }
