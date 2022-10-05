d26602621729ec447173b84f0658eec57cf4878f
Your Name <you@example.com>
2022-10-05 03:41:41 -0700

    debug

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,48 +11,45 @@
 int **alloc_grid(int width, int height)
 {
 
-int **grid, r, c;
+int **array;
+int i = 0, j;
 
-if (width <= 0 || height <= 0)
-{
-
+if (width == 0 || height == 0)
 return (NULL);
+array = (int **) malloc(sizeof(int *) * height);
+
+if (array != NULL)
+{
+
+for (; i < height; i++)
+{
+
+array[i] = (int *) malloc(sizeof(int) * width);
+if (array[i] != NULL)
+{
+
+for (j = 0; j < width; j++)
+array[i][j] = 0;
 
 }
+else
+{
 
-grid = malloc(sizeof(int *) * height);
-
-/*if memory is insifficuent*/
-
-if (!grid)
+while (i >= 0)
+{
+free(array[i]);
+i--;
+}
+free(array);
 return (NULL);
+}
 
-c = 0;
-while (c < height)
+}
+return (array);
+}
+else
 {
-
-*(grid + c) = malloc(width * sizeof(int));
-
-if (!(*(grid + c)))
-{
-
-while (c--)
-free(*(grid + c));
-free(grid);
 return (NULL);
-
 }
-r = 0;
-while (r < width)
-{
-
-*(*(grid + c) + r) = 0;
-r++;
-
-}
-c++;
-
-}
-return (grid);
 
 }
