453f7421554dce5a461d94e5ae9a51ca54390ca5
FTarus <111076129+FTarus@users.noreply.github.com>
2023-01-06 11:12:15 +0300

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,51 +1,72 @@
 #include "hash_tables.h"
+
 /**
-* hash_table_set - adds an element to a hash table
-* @ht: the table you want to add to
-* @key: the key
-* @value: the value
-* Return: 1 on success, 0 otherwise
-*/
+ * replace_node_value - set value of nodes with same key string
+ * @target: pointer to node target to change value
+ * @value: value to set in node
+ *
+ * Return: 1 if success, 0 if failure
+ */
+int replace_node_value(hash_node_t *target, const char *value)
+{
+	/* free the target node's current value */
+	/* strdup() the new value and set it in node */
+	free(target->value);
+	target->value = strdup(value);
+	if (target->value == NULL)
+		return (0);
+	return (1);
+}
+
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: pointer to hash table
+ * @key: input key as string
+ * @value: input value as string
+ *
+ * Return: 1 if success, 0 if failure
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *new = NULL, *temp = NULL;
+	unsigned long int key_idx;
+	hash_node_t **array = NULL;
+	hash_node_t *seeker = NULL;
+	hash_node_t *new_next = NULL;
+	hash_node_t *new_node = NULL;
 
-	if (key == NULL || key == '\0' || value == NULL || ht == NULL)
-	{
+	/* check if caller passing admissable arguments */
+	if (!ht || !key || !*key || !value)
 		return (0);
-	}
-	index = key_index((const unsigned char *)key, (*ht).size);
-	temp = (*ht).array[index];
-	while (temp != NULL)
+	array = ht->array;
+	key_idx = key_index((unsigned char *)key, ht->size);
+	if (array[key_idx])
 	{
-		if (strcmp((*temp).key, key) == 0)
+		/* traverse linked list for key == array[key_idx]->key */
+		seeker = array[key_idx];
+		while (seeker)
 		{
-			free((*temp).value);
-			(*temp).value = strdup(value);
-			return (1);
+			if (strcmp(seeker->key, key) == 0)
+			{
+				replace_node_value(seeker, value);
+				return (1);
+			}
+			seeker = seeker->next;
 		}
-		temp = (*temp).next;
+		/* if list isn't empty and key string unique set new_next */
+		new_next = array[key_idx];
 	}
-	new = malloc(sizeof(hash_node_t));
-	if (!new)
-	{
+	else
+		/* if list empty set new_next to NULL */
+		new_next = NULL;
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
 		return (0);
-	}
-	(*new).key = strdup(key);
-	if (!(*new).key)
-	{
-		free(new);
-		return (0);
-	}
-	(*new).value = strdup(value);
-	if (!(*new).value)
-	{
-		free((*new).key);
-		free(new);
-		return (0);
-	}
-	(*new).next = (*ht).array[index];
-	(*ht).array[index] = new;
+	/* duplicate and set value and key */
+	new_node->value = strdup(value);
+	new_node->key = strdup(key);
+	new_node->next = new_next;
+	/* set linked list head to new_node */
+	array[key_idx] = new_node;
 	return (1);
 }
