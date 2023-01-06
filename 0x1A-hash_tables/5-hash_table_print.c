2edac7567d9b92276ada3da2436a2154816ae186
FTarus <111076129+FTarus@users.noreply.github.com>
2023-01-06 11:41:53 +0300

    Update 5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,34 +1,45 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print - prints a hash table
- * @ht: the hash table being printed
+ * hash_table_print - function that prints a hash table
+ * @ht: pointer to hash table
+ *
+ * Return: void
  */
-
 void hash_table_print(const hash_table_t *ht)
 {
-	hash_node_t *temp;
-	int delim = 98;
-	unsigned long int index;
+	hash_node_t **array = NULL;
+	hash_node_t *seeker = NULL;
+	size_t print_comma_flag = 0;
+	size_t i = 0;
 
-	if (ht == NULL || (*ht).array == NULL)
-	{
+	/* check if ht is NULL return */
+	if (!ht)
 		return;
-	}
+
+	array = ht->array;
+
 	printf("{");
-	for (index = 0; (*ht).size > index; index++)
+	/* loop through indices of array for non-NULL */
+	while (i < ht->size)
 	{
-		temp = (*ht).array[index];
-		while (temp != NULL)
+		if (array[i])
 		{
-			if (delim == 1)
+			/* valid pointer found */
+			print_comma_flag = 1;
+			seeker = array[i];
+			while (seeker)
 			{
-				printf(", ");
+				printf("'%s': ", seeker->key);
+				printf("'%s'", seeker->value);
+				if (seeker->next)
+					printf(", ");
+				seeker = seeker->next;
 			}
-			printf("'%s': '%s'", (*temp).key, (*temp).value);
-			temp = (*temp).next;
-			delim = 1;
 		}
+		if (array[i + 1] && print_comma_flag)
+			printf(", ");
+		i++;
 	}
 	printf("}\n");
 }
