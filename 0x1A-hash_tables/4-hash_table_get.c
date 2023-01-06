30e66f23e88f0573d31e81ef9d1388bcc4df6e4e
FTarus <111076129+FTarus@users.noreply.github.com>
2023-01-06 11:40:34 +0300

    Update 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,24 +1,40 @@
 #include "hash_tables.h"
+
 /**
-* hash_table_get - gets a hash table
-* @ht: the hast table bein retieved from
-* @key: the key used for retrieving
-* Return: Value, NULL if key not found
-*/
+ * hash_table_get - retrieve a value based on a given key
+ * @ht: pointer to hash table
+ * @key: input key as string
+ *
+ * Return: value associated with the element or NULL if key not found
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index = 0;
-	hash_node_t *Head;
+	unsigned long int key_idx;
+	hash_node_t **array = NULL;
+	hash_node_t *seeker = NULL;
 
-	if (key == '\0' || key == NULL || ht == NULL)
+	/* check if caller passing admissable arguments */
+	if (!ht || !key || !*key)
 		return (NULL);
-	index = key_index((unsigned char *)key, (*ht).size);
-	Head = (*ht).array[index];
-	while (Head != NULL)
+
+	array = ht->array;
+	/* retrieve hashed key_idx */
+	key_idx = key_index((unsigned char *)key, ht->size);
+
+	/* check if key_idx pointer to node */
+	if (!array[key_idx])
+		return (NULL);
+
+	seeker = array[key_idx];
+	/* loop through matched key slot for matching key string in llist */
+	while (seeker)
 	{
-		if (strcmp((*Head).key, key) == 0)
-			return ((*Head).value);
-		Head = (*Head).next;
+		if (strcmp(seeker->key, key) == 0)
+			return (seeker->value);
+		seeker = seeker->next;
 	}
+
+	/* key not found */
 	return (NULL);
+
 }
